4a61d197f0806d16ddb5ee7e3536689af052a0cb
johnny-rob <jpinney1@gmail.com>
2021-06-10 07:19:07 -0400

    Finished implmenting stripe payment system, resolved bugs

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -6,8 +6,12 @@ class CheckoutsController < ApplicationController
     current_user.customer
 
     @checkout_session = current_user.payment_processor.checkout(
-      mode: "payment",
-      line_items:"price_1Ivi7kK0BnqKq6sDKFTewPXz"
+      #Single Payment
+        #mode: "payment",
+        #line_items:"price_1Ivi7kK0BnqKq6sDKFTewPXz"
+      #Subscription payment
+        mode: "subscription",
+        line_items: "price_1J04DhK0BnqKq6sDvrNm18GZ"
     )
   end
 end
